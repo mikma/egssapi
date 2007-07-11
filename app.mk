01d8324a4c3151fe72776d2fb1f54f925c44e454
Mikael Magnusson <mikma@users.sourceforge.net>
2007-07-11 07:54:11 +0000

    Rename OPT -> OTP, more
    git-svn-id: svn+ssh://svn.hem.za.org/srv/svn/repository/egssapi/trunk@659 ca8862df-6cf8-0310-9cd1-bd52f9e4cbda

--- app.mk
+++ app.mk
@@ -1,4 +1,4 @@
-libsubdir = $(ERLANG_INSTALL_LIB_DIR)/$(OTP_APP)-$($(OPT_APP)_VSN)
+libsubdir = $(ERLANG_INSTALL_LIB_DIR)/$(OTP_APP)-$($(OTP_APP)_VSN)
 bindir = $(libsubdir)/bin
 ebindir = $(libsubdir)/ebin
 incdir = $(libsubdir)/include
@@ -7,7 +7,7 @@ inc_HEADERS = $($(OTP_APP)_HDRS)
 beam_FILES = $($(OTP_APP)_SRCS:.erl=.beam)
 
 ebin_DATA = $(beam_FILES) $(OTP_APP:=.app)
-EXTRA_DIST = $($(OTP_APP)_SRCS) $(OPT_APP:=.app-in)
+EXTRA_DIST = $($(OTP_APP)_SRCS) $(OTP_APP:=.app-in)
 CLEANFILES = $(beam_FILES) $(OTP_APP:=.app)
 
 include $(top_srcdir)/rules.mk
--- rel.mk
+++ rel.mk
@@ -1,12 +1,12 @@
-libsubdir = $(ERLANG_INSTALL_LIB_DIR)/$(OTP_APP)-$($(OPT_APP)_VSN)
+libsubdir = $(ERLANG_INSTALL_LIB_DIR)/$(OTP_APP)-$($(OTP_APP)_VSN)
 bindir = $(libsubdir)/bin
 ebindir = $(libsubdir)/ebin
 incdir = $(libsubdir)/include
 
-ebin_DATA = $(OTP_RELEASES:=.boot) $(OPT_RELEASES:=.rel)	\
+ebin_DATA = $(OTP_RELEASES:=.boot) $(OTP_RELEASES:=.rel)	\
 $(OTP_RELEASES:=.script)
 EXTRA_DIST = $(OTP_RELEASES:=.rel.in)
-CLEANFILES = $(OTP_RELEASES:=.boot) $(OPT_RELEASES:=.rel)	\
+CLEANFILES = $(OTP_RELEASES:=.boot) $(OTP_RELEASES:=.rel)	\
 $(OTP_RELEASES:=.script)
 
 include $(top_srcdir)/rules.mk
