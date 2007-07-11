1a98836c562b198a773bf9b11aacfa4746f68b5e
Mikael Magnusson <mikma@users.sourceforge.net>
2007-07-11 07:51:14 +0000

    Fix app VSN substitution
    git-svn-id: svn+ssh://svn.hem.za.org/srv/svn/repository/egssapi/trunk@657 ca8862df-6cf8-0310-9cd1-bd52f9e4cbda

--- rules.mk
+++ rules.mk
@@ -1,12 +1,15 @@
+SUBST = sed $(ERLANG_LIB_VER_SUBST) \
+	-e 's/[@]$(OPT_APP)_VSN[@]/$($(OPT_APP)_VSN)/'
+
 %.rel: %.rel.in
-	sed $(ERLANG_LIB_VER_SUBST) $< > $@
+	$(SUBST) $< > $@
 
 %.beam: %.erl
 	@echo [ERLC] $@
 	@$(ERLC) $(AM_ERL_FLAGS) $(ERL_FLAGS) $(AM_ERLCFLAGS) $(ERLCFLAGS) $<
 
 %.app: %.app-in
-	sed $(ERLANG_LIB_VER_SUBST) $< > $@
+	$(SUBST) $< > $@
 
 %.boot: %.rel %.app
 	@echo [ERLC] $@
