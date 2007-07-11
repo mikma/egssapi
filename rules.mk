f54f5fe7b5db2da93f0eec04604d9974772f3441
Mikael Magnusson <mikma@users.sourceforge.net>
2007-07-11 07:42:44 +0000

    Autoconf sed command line containing dependent libraries
    git-svn-id: svn+ssh://svn.hem.za.org/srv/svn/repository/egssapi/trunk@656 ca8862df-6cf8-0310-9cd1-bd52f9e4cbda

--- rules.mk
+++ rules.mk
@@ -1,19 +1,12 @@
-SUBST = sed \
-	-e 's,[@]ERLANG_LIB_VER_kernel[@],$(ERLANG_LIB_VER_kernel),g' \
-	-e 's,[@]ERLANG_LIB_VER_stdlib[@],$(ERLANG_LIB_VER_stdlib),g' \
-	-e 's,[@]ERLANG_LIB_VER_sasl[@],$(ERLANG_LIB_VER_sasl),g' \
-	-e 's,[@]ERLANG_LIB_VER_crypto[@],$(ERLANG_LIB_VER_crypto),g' \
-	-e 's,[@]VSN[@],$($(OPT_APP)_VSN),g'
-
 %.rel: %.rel.in
-	$(SUBST) $< > $@
+	sed $(ERLANG_LIB_VER_SUBST) $< > $@
 
 %.beam: %.erl
 	@echo [ERLC] $@
 	@$(ERLC) $(AM_ERL_FLAGS) $(ERL_FLAGS) $(AM_ERLCFLAGS) $(ERLCFLAGS) $<
 
 %.app: %.app-in
-	$(SUBST) $< > $@
+	sed $(ERLANG_LIB_VER_SUBST) $< > $@
 
 %.boot: %.rel %.app
 	@echo [ERLC] $@
